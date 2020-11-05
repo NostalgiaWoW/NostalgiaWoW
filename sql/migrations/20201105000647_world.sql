47c6a5b57f1a4920f816d13ed2260be1b2fb049d
GrenderG <grenderg@gmail.com>
2020-11-05 04:43:44 +0100

    Typo.

--- sql/migrations/20201105000647_world.sql
+++ sql/migrations/20201105000647_world.sql
@@ -9,7 +9,7 @@ INSERT INTO `migrations` VALUES ('20201105000647');
 -- Add your query below.
 
 update creature_template set minhealth = minhealth*0.50, maxhealth = maxhealth*0.50, mindmg = mindmg*0.8, maxdmg = maxdmg*0.8 where rank > 0 and entry in (select id from creature where map = 30);
-update battleground_template set MinPlayersPerTeam = 5 and MaxPlayersPerTeam = 20 where id = 1;
+update battleground_template set MinPlayersPerTeam = 5, MaxPlayersPerTeam = 20 where id = 1;
 
 replace into `creature_template` (`entry`, `KillCredit1`, `KillCredit2`, `modelid_1`, `modelid_2`, `modelid_3`, `modelid_4`, `name`, `subname`, `gossip_menu_id`, `minlevel`, `maxlevel`, `minhealth`, `maxhealth`, `minmana`, `maxmana`, `armor`, `faction_A`, `faction_H`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `PetSpellDataId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `Civilian`, `RacialLeader`, `RegenHealth`, `equipment_id`, `trainer_id`, `vendor_id`, `MechanicImmuneMask`, `SchoolImmuneMask`, `flags_extra`, `ScriptName`)
 values
